c573165fa5747bb4cdf3172a1917fb6c4dd021bb
cynthia.quach <ccquach93@gmail.com>
2018-08-20 21:45:53 -0700

    dynamically set styles and classes

--- react-app-demo/src/App.js
+++ react-app-demo/src/App.js
@@ -43,7 +43,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -66,14 +67,20 @@ class App extends Component {
           ))}
         </div>
       );
+
+      style.backgroundColor = 'red';
     }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) classes.push('red'); // classes = ['red']
+    if (this.state.persons.length <= 1) classes.push('bold'); // classes = ['red', 'bold']
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button style={style} onClick={this.togglePersonsHandler}>
-          Switch Name
+          Toggle Persons
         </button>
         {persons}
       </div>
