3193c468b6cfd93ff5556c6793048633c8f5a662
cynthia.quach <ccquach93@gmail.com>
2018-08-22 14:36:39 -0700

    prevent order summary from updating when modal not open

--- 01-burger-builder-basic/src/components/Burger/OrderSummary/OrderSummary.js
+++ 01-burger-builder-basic/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Auxiliary from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = props => {
-  const ingredientsSummary = Object.keys(props.ingredients).map(igKey => (
-    <li key={igKey}>
-      <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
-      {props.ingredients[igKey]}
-    </li>
-  ));
-  return (
-    <Auxiliary>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientsSummary}</ul>
-      <p>
-        <strong>Total Price: ${props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCanceled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Auxiliary>
-  );
-};
+/*
+This could be functional component. Converted to container for debugging purposes.
+*/
+class OrderSummary extends Component {
+  componentWillUpdate = () => {
+    console.log(`[OrderSummary] willUpdate`);
+  };
+
+  render() {
+    const ingredientsSummary = Object.keys(this.props.ingredients).map(
+      igKey => (
+        <li key={igKey}>
+          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+          {this.props.ingredients[igKey]}
+        </li>
+      )
+    );
+    return (
+      <Auxiliary>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientsSummary}</ul>
+        <p>
+          <strong>Total Price: ${this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCanceled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Auxiliary>
+    );
+  }
+}
 
 OrderSummary.propTypes = {
   ingredients: PropTypes.shape({
--- 01-burger-builder-basic/src/components/UI/Modal/Modal.js
+++ 01-burger-builder-basic/src/components/UI/Modal/Modal.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './Modal.css';
 import Auxiliary from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = props => (
-  <Auxiliary>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? 1 : 0
-      }}
-    >
-      {props.children}
-    </div>
-  </Auxiliary>
-);
+class Modal extends Component {
+  /* 
+  Prevent update of OrderSummary if modal not showing.
+  Not extending PureComponent because it would execute more checks than needed.
+  */
+  shouldComponentUpdate = (nextProps, nextState) => {
+    return nextProps.show !== this.props.show;
+  };
+
+  componentWillUpdate = (nextProps, nextState) => {
+    console.log(`[Modal] willUpdate`);
+  };
+
+  render() {
+    return (
+      <Auxiliary>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? 1 : 0
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Auxiliary>
+    );
+  }
+}
 
 Modal.propTypes = {
   children: PropTypes.element.isRequired,
