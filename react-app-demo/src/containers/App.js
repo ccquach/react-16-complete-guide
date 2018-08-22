7aec4a20eacdb6627ad52bc8e37f59e7bae9ae14
cynthia.quach <ccquach93@gmail.com>
2018-08-21 17:24:10 -0700

    update state based on previous state using function param

--- react-app-demo/src/containers/App.js
+++ react-app-demo/src/containers/App.js
@@ -15,7 +15,8 @@ class App extends PureComponent {
         { id: '38dj9fdw', name: 'Stephanie', age: 26 }
       ],
       otherState: 'some other value',
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0
     };
     console.log(`[App.js] Inside Contructor`, props);
   }
@@ -88,7 +89,10 @@ class App extends PureComponent {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
+    this.setState((prevState, props) => ({
+      showPersons: !doesShow,
+      toggleClicked: prevState.toggleClicked + 1
+    }));
   };
 
   render() {
