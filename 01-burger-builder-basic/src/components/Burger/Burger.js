403a4eea6735842f91f3746d582bee461f874660
cynthia.quach <ccquach93@gmail.com>
2018-08-21 23:05:01 -0700

    display message if no ingredients added

--- 01-burger-builder-basic/src/components/Burger/Burger.js
+++ 01-burger-builder-basic/src/components/Burger/Burger.js
@@ -5,11 +5,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey =>
-    [...Array(props.ingredients[igKey])].map((_, i) => (
-      <BurgerIngredient key={igKey + i} type={igKey} />
-    ))
-  );
+  // transform ingredients object to array
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey =>
+      [...Array(props.ingredients[igKey])].map((_, i) => (
+        <BurgerIngredient key={igKey + i} type={igKey} />
+      ))
+    )
+    .reduce((arr, el) => arr.concat(el), []);
+
+  // display message if no ingredients added
+  if (transformedIngredients.length === 0)
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
--- 01-burger-builder-basic/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 01-burger-builder-basic/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,10 +6,10 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   };
 
