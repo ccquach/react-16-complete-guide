7f8c6d07520ff2be9d6e2dd77d83cca9a814ed11
cynthia.quach <ccquach93@gmail.com>
2018-08-21 22:56:13 -0700

    add dynamic rendering of burger ingredients

--- 01-burger-builder-basic/src/components/Burger/Burger.js
+++ 01-burger-builder-basic/src/components/Burger/Burger.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const Burger = () => {
+const Burger = props => {
+  const transformedIngredients = Object.keys(props.ingredients).map(igKey =>
+    [...Array(props.ingredients[igKey])].map((_, i) => (
+      <BurgerIngredient key={igKey + i} type={igKey} />
+    ))
+  );
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
+};
+
+Burger.propTypes = {
+  ingredients: PropTypes.shape({
+    salad: PropTypes.number.isRequired,
+    bacon: PropTypes.number.isRequired,
+    cheese: PropTypes.number.isRequired,
+    meat: PropTypes.number.isRequired
+  }).isRequired
 };
 
 export default Burger;
--- 01-burger-builder-basic/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 01-burger-builder-basic/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,19 @@ import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  };
+
   render() {
     return (
       <Auxiliary>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Auxiliary>
     );
