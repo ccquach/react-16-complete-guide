0f526ed817eb07859c29270e5b9acc53ef8c61a5
cynthia.quach <ccquach93@gmail.com>
2018-08-22 14:21:36 -0700

    add navigation toolbar and side drawer

--- 01-burger-builder-basic/src/components/Layout/Layout.js
+++ 01-burger-builder-basic/src/components/Layout/Layout.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Layout.css';
 import Auxiliary from '../../hoc/Auxiliary';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = props => (
-  <Auxiliary>
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>{props.children}</main>
-  </Auxiliary>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  };
+
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => ({ showSideDrawer: !prevState.showSideDrawer }));
+  };
+
+  render() {
+    return (
+      <Auxiliary>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          closed={this.sideDrawerToggleHandler}
+          open={this.state.showSideDrawer}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Auxiliary>
+    );
+  }
+}
 
 export default Layout;
