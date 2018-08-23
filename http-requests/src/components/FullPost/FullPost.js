4c5979bc4764fe3227b132a90bf77ee2085fb998
cynthia.quach <ccquach93@gmail.com>
2018-08-22 22:05:43 -0700

    fetch and display data for selected post

--- http-requests/src/components/FullPost/FullPost.js
+++ http-requests/src/components/FullPost/FullPost.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+  state = {
+    loadedPost: null
+  };
 
-        );
-        return post;
+  componentDidUpdate = (prevProps, prevState) => {
+    // fetch data for selected Id if not null
+    if (this.props.id) {
+      /*
+      Calling setState inside componentDidUpdate causes infinite loop, so
+      if post loaded, check that props id differs from current loaded
+      post's id before sending another GET request. If no post is loaded,
+      fetch the data.
+      */
+      if (
+        !this.state.loadedPost ||
+        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+      ) {
+        axios
+          .get(`http://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+          .then(res => this.setState({ loadedPost: res.data }));
+      }
     }
+  };
+
+  render() {
+    const loadedPost = this.state.loadedPost;
+    let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
+    // on first render, we get Id but fetch not completed yet
+    if (this.props.id) post = <p style={{ textAlign: 'center' }}>Loading...</p>;
+    // component re-rendered with updated stated once fetch completed
+    if (loadedPost) {
+      post = (
+        <div className="FullPost">
+          <h1>{loadedPost.title}</h1>
+          <p>{loadedPost.body}</p>
+          <div className="Edit">
+            <button className="Delete">Delete</button>
+          </div>
+        </div>
+      );
+    }
+    return post;
+  }
 }
+
+FullPost.propTypes = {
+  id: PropTypes.number
+};
 
 export default FullPost;
--- http-requests/src/containers/Blog/Blog.js
+++ http-requests/src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   };
 
   componentDidMount = () => {
@@ -19,16 +20,25 @@ class Blog extends Component {
     });
   };
 
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
+
   render() {
     const posts = this.state.posts.map(post => (
-      <Post key={post.id} title={post.title} author={post.author} />
+      <Post
+        key={post.id}
+        title={post.title}
+        author={post.author}
+        clicked={this.postSelectedHandler.bind(this, post.id)}
+      />
     ));
 
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
