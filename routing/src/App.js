4f8987399880b9a670d97c647739238197c62ede
cynthia.quach <ccquach93@gmail.com>
2018-08-23 23:39:39 -0700

    deployment config

--- routing/src/App.js
+++ routing/src/App.js
@@ -6,6 +6,11 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      /*
+      If production app served on sub-domain need to make React 
+      aware that basename is not the default root domain "/"
+      */
+      // <Router basename="/my-app">
       <Router>
         <div className="App">
           <Blog />
