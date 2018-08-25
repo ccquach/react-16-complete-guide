a41297c94c219965277a4b96dfcc8ccc3ce7fad9
cynthia.quach <ccquach93@gmail.com>
2018-08-25 10:44:40 -0700

    react-redux starter

---
+++ redux/src/components/CounterOutput/CounterOutput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './CounterOutput.css';
+
+const counterOutput = props => (
+  <div className="CounterOutput">Current Counter: {props.value}</div>
+);
+
+export default counterOutput;
