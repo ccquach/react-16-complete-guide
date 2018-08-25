2cb7b8b01607072cce0fb0cf3e6028cef5371f7e
cynthia.quach <ccquach93@gmail.com>
2018-08-25 11:17:05 -0700

    connect app to redux store

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './store/reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create redux store
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  // Connect app to redux store
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
