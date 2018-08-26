e98bc3c41239d2e112e755aeb7156f5a75417392
cynthia.quach <ccquach93@gmail.com>
2018-08-26 14:07:12 -0700

    refactor reducers to clean up switch statements

--- 05-burger-builder-redux/src/store/reducers/burgerBuilder.js
+++ 05-burger-builder-redux/src/store/reducers/burgerBuilder.js
@@ -13,50 +13,58 @@ const INGREDIENT_PRICES = {
   bacon: 0.7
 };
 
+const addIngredient = (state, action) => ({
+  ...state,
+  ingredients: {
+    ...state.ingredients,
+    [action.name]: state.ingredients[action.name] + 1
+  },
+  totalPrice: state.totalPrice + INGREDIENT_PRICES[action.name]
+});
+
+const removeIngredient = (state, action) => ({
+  ...state,
+  ingredients: {
+    ...state.ingredients,
+    [action.name]: state.ingredients[action.name] - 1
+  },
+  totalPrice: state.totalPrice - INGREDIENT_PRICES[action.name]
+});
+
+const setIngredients = (state, action) => ({
+  ...state,
+  /* 
+  Default alphabetical order of display determined
+  by data returned from Firebase.
+  Hard code ingredients to desired order of display.
+  Limits flexibility of number of ingredients, but 
+  already limited by CSS code being used
+  */
+  ingredients: {
+    bacon: action.ingredients.bacon,
+    cheese: action.ingredients.cheese,
+    salad: action.ingredients.salad,
+    meat: action.ingredients.meat
+  },
+  totalPrice: initialState.totalPrice,
+  error: false
+});
+
+const fetchIngredientsFailed = state => ({
+  ...state,
+  error: true
+});
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.name]: state.ingredients[action.name] + 1
-        },
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.name]
-      };
+      return addIngredient(state, action);
     case actionTypes.REMOVE_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.name]: state.ingredients[action.name] - 1
-        },
-        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.name]
-      };
+      return removeIngredient(state, action);
     case actionTypes.SET_INGREDIENTS:
-      return {
-        ...state,
-        /* 
-        Default alphabetical order of display determined
-        by data returned from Firebase.
-        Hard code ingredients to desired order of display.
-        Limits flexibility of number of ingredients, but 
-        already limited by CSS code being used
-        */
-        ingredients: {
-          bacon: action.ingredients.bacon,
-          cheese: action.ingredients.cheese,
-          salad: action.ingredients.salad,
-          meat: action.ingredients.meat
-        },
-        totalPrice: initialState.totalPrice,
-        error: false
-      };
+      return setIngredients(state, action);
     case actionTypes.FETCH_INGREDIENTS_FAILED:
-      return {
-        ...state,
-        error: true
-      };
+      return fetchIngredientsFailed(state);
     default:
       return state;
   }
--- 05-burger-builder-redux/src/store/reducers/order.js
+++ 05-burger-builder-redux/src/store/reducers/order.js
@@ -6,38 +6,48 @@ const initialState = {
   purchased: false
 };
 
+const purchaseInit = state => ({
+  ...state,
+  purchased: false
+});
+
+const start = state => ({
+  ...state,
+  loading: true
+});
+
+const purchaseBurgerSuccess = (state, action) => ({
+  ...state,
+  loading: false,
+  purchased: true,
+  orders: [...state.orders, { ...action.orderData, id: action.orderId }]
+});
+
+const fetchOrdersSuccess = (state, action) => ({
+  ...state,
+  orders: action.orders,
+  loading: false
+});
+
+const fail = state => ({
+  ...state,
+  loading: false
+});
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.PURCHASE_INIT:
-      return {
-        ...state,
-        purchased: false
-      };
+      return purchaseInit(state);
     case actionTypes.PURCHASE_BURGER_START:
     case actionTypes.FETCH_ORDERS_START:
-      return {
-        ...state,
-        loading: true
-      };
+      return start(state);
     case actionTypes.PURCHASE_BURGER_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        purchased: true,
-        orders: [...state.orders, { ...action.orderData, id: action.orderId }]
-      };
+      return purchaseBurgerSuccess(state, action);
     case actionTypes.FETCH_ORDERS_SUCCESS:
-      return {
-        ...state,
-        orders: action.orders,
-        loading: false
-      };
+      return fetchOrdersSuccess(state, action);
     case actionTypes.PURCHASE_BURGER_FAIL:
     case actionTypes.FETCH_ORDERS_FAIL:
-      return {
-        ...state,
-        loading: false
-      };
+      return fail(state);
     default:
       return state;
   }
