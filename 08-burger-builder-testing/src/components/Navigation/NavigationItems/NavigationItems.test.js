8d43906e653a22a40ba5037f42cb283e217abb44
cynthia.quach <ccquach93@gmail.com>
2018-08-28 11:54:19 -0700

    multiple tests on the same test bundle

--- 08-burger-builder-testing/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ 08-burger-builder-testing/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -8,11 +8,20 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({ adapter: new Adapter() });
 
 describe('<NavigationItems />', () => {
+  // instead of duplicating method calls for similar tests
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
+  // set props on rendered component for each unique case
   it('should render two <NavigationItem /> elements if not authenticated', () => {
-    // if isAuthenticated prop not passed, it is treated as false
-    const wrapper = shallow(<NavigationItems isAuthenticated={false} />);
-    // Checks that it renders NavigationItem component
-    // Expects it to render two NavigationItem components
+    wrapper.setProps({ isAuthenticated: false });
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it('should render three <NavigationItem /> elements if authenticated', () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
