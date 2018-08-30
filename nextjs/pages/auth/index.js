7af0d213b5bdd67f62c05f8f1f19e7d686b1c6af
cynthia.quach <ccquach93@gmail.com>
2018-08-29 22:07:45 -0700

    setup project

---
+++ nextjs/pages/auth/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const authIndexPage = () => {
+  return (
+    <div>
+      <h1>The Auth Index Page</h1>
+    </div>
+  );
+};
+
+export default authIndexPage;
---
+++ nextjs/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const indexPage = () => {
+  return (
+    <div>
+      <h1>The Main Page</h1>
+    </div>
+  );
+};
+
+export default indexPage;
