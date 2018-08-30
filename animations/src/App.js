069c21f70e6a6c67a7c5989c8ddecf591211bfb3
cynthia.quach <ccquach93@gmail.com>
2018-08-30 11:37:31 -0700

    using CSS animations

--- animations/src/App.js
+++ animations/src/App.js
@@ -17,8 +17,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        <Backdrop show={this.state.modalIsOpen} />
+        {/* <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        <Backdrop show={this.state.modalIsOpen} /> */}
+        {this.state.modalIsOpen ? (
+          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        ) : null}
+        {this.state.modalIsOpen ? (
+          <Backdrop show={this.state.modalIsOpen} />
+        ) : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
