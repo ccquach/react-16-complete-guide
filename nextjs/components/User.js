f7c8060be0c50c6dd95e75e8f656aebe864f9aa8
cynthia.quach <ccquach93@gmail.com>
2018-08-30 09:16:18 -0700

    styling components

--- nextjs/components/User.js
+++ nextjs/components/User.js
@@ -5,6 +5,14 @@ const User = props => {
     <div>
       <h1>{props.name}</h1>
       <p>Age: {props.age}</p>
+      <style jsx>{`
+        div {
+          border: 1px solid #eee;
+          box-shadow: 0 2px 3px #ccc;
+          padding: 20px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 };
