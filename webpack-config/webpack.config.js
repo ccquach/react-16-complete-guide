c2e334f124eff9421886893d0fa201d51c5992bd
cynthia.quach <ccquach93@gmail.com>
2018-08-29 20:15:37 -0700

    inject output script into index.html

--- webpack-config/webpack.config.js
+++ webpack-config/webpack.config.js
@@ -2,6 +2,7 @@
 const path = require('path');
 
 const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -93,4 +94,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
 };
