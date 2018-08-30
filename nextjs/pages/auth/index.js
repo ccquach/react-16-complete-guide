3aba332a073d13eb07c07b578e05cdd50f4c6faa
cynthia.quach <ccquach93@gmail.com>
2018-08-30 10:04:32 -0700

    getInitialProps lifecycle hook

--- nextjs/pages/auth/index.js
+++ nextjs/pages/auth/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 
 import User from '../../components/User';
 
-const authIndexPage = () => {
+const authIndexPage = props => {
   return (
     <div>
-      <h1>The Auth Index Page</h1>
+      <h1>The Auth Index Page - {props.appName}</h1>
       <User name="Max" age={28} />
     </div>
   );
+};
+
+authIndexPage.getInitialProps = context => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ appName: 'Super App Auth' });
+    }, 1000);
+  });
+  return promise;
 };
 
 export default authIndexPage;
--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 
-const indexPage = () => {
-  return (
-    <div>
-      <h1>The Main Page</h1>
-      <p>
-        Go to{' '}
-        <Link href="/auth">
-          <a>Auth</a>
-        </Link>
-      </p>
-      <button onClick={() => Router.push('/auth')}>Go to Auth</button>
-    </div>
-  );
-};
+class indexPage extends Component {
+  static getInitialProps(context) {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ appName: 'Super App' });
+      }, 1000);
+    });
+    return promise;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The Main Page of {this.props.appName}</h1>
+        <p>
+          Go to{' '}
+          <Link href="/auth">
+            <a>Auth</a>
+          </Link>
+        </p>
+        <button onClick={() => Router.push('/auth')}>Go to Auth</button>
+      </div>
+    );
+  }
+}
 
 export default indexPage;
