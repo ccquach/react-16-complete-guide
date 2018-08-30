5c9b6327354d11e61385506040a8df6c6b7cdbd4
cynthia.quach <ccquach93@gmail.com>
2018-08-30 13:58:44 -0700

    transition events

--- animations/src/App.js
+++ animations/src/App.js
@@ -33,6 +33,12 @@ class App extends Component {
           timeout={300}
           mountOnEnter
           unmountOnExit
+          onEnter={() => console.log(`onEnter`)}
+          onEntering={() => console.log(`onEntering`)}
+          onEntered={() => console.log(`onEntered`)}
+          onExit={() => console.log(`onExit`)}
+          onExiting={() => console.log(`onExiting`)}
+          onExited={() => console.log(`onExited`)}
         >
           {state => (
             <div
