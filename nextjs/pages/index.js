fbebdabfb14ffa660a79e501d0f387e1d4807b2e
cynthia.quach <ccquach93@gmail.com>
2018-08-29 22:24:52 -0700

    navigating routes

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Link from 'next/link';
+import Router from 'next/router';
 
 const indexPage = () => {
   return (
     <div>
       <h1>The Main Page</h1>
+      <p>
+        Go to{' '}
+        <Link href="/auth">
+          <a>Auth</a>
+        </Link>
+      </p>
+      <button onClick={() => Router.push('/auth')}>Go to Auth</button>
     </div>
   );
 };
